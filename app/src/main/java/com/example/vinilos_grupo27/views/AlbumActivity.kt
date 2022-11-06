9cb68bf537a694b43b068fd686ae63a19943aa4e
Nelson <n.ortizf@uniandes.edu.co>
2022-11-05 21:49:20 -0500

    activity album

---
+++ app/src/main/java/com/example/vinilos_grupo27/views/AlbumActivity.kt
@@ -0,0 +1,12 @@
+package com.example.vinilos_grupo27.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.vinilos_grupo27.R
+
+class AlbumActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_album)
+    }
+}
