3a54a2f221e6c35f434a3376384b348671b93d7c
Nelson <n.ortizf@uniandes.edu.co>
2022-11-06 10:45:30 -0500

    modelView musicians sin conexion a view

---
+++ app/src/main/java/com/example/vinilos_grupo27/viewmodels/MusicianViewModel.kt
@@ -0,0 +1,57 @@
+package com.example.vinilos_grupo27.viewmodels
+
+import android.app.Application
+import android.util.Log
+import androidx.lifecycle.*
+import com.example.vinilos_grupo27.models.Musician
+import com.example.vinilos_grupo27.repositories.MusiciansRepository
+
+class MusicianViewModel (application: Application) :  AndroidViewModel(application) {
+
+    private val musiciansRepository = MusiciansRepository(application)
+
+    private val _musicians = MutableLiveData<List<Musician>>()
+
+    val musicians: LiveData<List<Musician>>
+        get() = _musicians
+
+    private var _eventNetworkError = MutableLiveData<Boolean>(false)
+
+    val eventNetworkError: LiveData<Boolean>
+        get() = _eventNetworkError
+
+    private var _isNetworkErrorShown = MutableLiveData<Boolean>(false)
+
+    val isNetworkErrorShown: LiveData<Boolean>
+        get() = _isNetworkErrorShown
+
+    init {
+        refreshDataFromNetwork()
+    }
+
+    private fun refreshDataFromNetwork() {
+        musiciansRepository.refreshData({
+            _musicians.postValue(it)
+            _eventNetworkError.value = false
+            _isNetworkErrorShown.value = false
+        },{
+            Log.d("Error", it.toString())
+            _eventNetworkError.value = true
+        })
+    }
+
+    fun onNetworkErrorShown() {
+        _isNetworkErrorShown.value = true
+    }
+
+    class Factory(val app: Application) : ViewModelProvider.Factory {
+        override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+            if (modelClass.isAssignableFrom(MusicianViewModel::class.java)) {
+                @Suppress("UNCHECKED_CAST")
+                return MusicianViewModel(app) as T
+            }
+            throw IllegalArgumentException("Unable to construct viewmodel")
+        }
+    }
+
+}
