7d5597db1dabd50d4129833bd1cb8877efcb3658
Jhon Sebastian Bohorquez Guerrero <sebastian.bohorquez@ariadnacg.com>
2022-11-06 23:15:39 -0500

    Develop (#57)
    * gradle update
    
    * configuration files
    
    * Pantalla Albumes Capa View
    
    * activity album
    
    * fragmentos modificados
    
    * capa model para musicians
    
    * modelView musicians sin conexion a view
    
    * MusicianViewModel updated
    
    * Capa View artistas
    
    * permisos de internet, navegacion y visualizacion de musicos Ok
    
    * agrandar letras y organizar archivo musicianFragment en view
    
    * Creacion AlbumModel, NetworkServiceAdapter, Album
    
    Repository, AlbumViewModel
    
    * Borrando codigo duplicado
    
    * Update navigation file
    
    * Initial design for home view
    
    * Listar artistas, Network, adaptes, fragment, nav
    
    * Feature artistas (#51)
    
    * gradle update
    
    * capa model para musicians
    
    * modelView musicians sin conexion a view
    
    * MusicianViewModel updated
    
    * Capa View artistas
    
    * permisos de internet, navegacion y visualizacion de musicos Ok
    
    * agrandar letras y organizar archivo musicianFragment en view
    
    * Update navigation file
    
    * Initial design for home view
    
    Co-authored-by: Nelson <n.ortizf@uniandes.edu.co>
    Co-authored-by: Sebastian Bohorquez <sebastian.bohorquez@ariadnacg.com>
    
    * Design artists list view
    
    * Revert "Merge Feature listar albumes" (#53)
    
    * problem two buttons
    
    * gradle update
    
    * Revert ultimo push
    
    * view firts page
    
    * Pruebas Monkey
    
    * Design albums view
    
    Co-authored-by: Nelson <n.ortizf@uniandes.edu.co>
    Co-authored-by: DaMoAndes <d.moralesa2@uniandes.edu.co>
    Co-authored-by: Krishtian Barrera <kd.barrera@uniandes.edu.co>

---
+++ app/src/androidTest/java/com/example/vinilos_grupo27/MainActivityTest.kt
@@ -0,0 +1,58 @@
+package com.example.vinilos_grupo27
+
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.*
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.core.IsInstanceOf
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+
+    @Rule
+    @JvmField
+    var mActivityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun mainActivityTest() {
+        val button = onView(
+            allOf(
+                withId(R.id.button_first), withText("ARTISTAS"),
+                withParent(withParent(withId(R.id.nav_host_fragment_content_main))),
+                isDisplayed()
+            )
+        )
+        button.check(matches(isDisplayed()))
+
+        val button2 = onView(
+            allOf(
+                withId(R.id.button_second), withText("ÁLBUMES"),
+                withParent(withParent(withId(R.id.nav_host_fragment_content_main))),
+                isDisplayed()
+            )
+        )
+        button2.check(matches(isDisplayed()))
+
+        val linearLayoutCompat = onView(
+            allOf(
+                withParent(
+                    allOf(
+                        withId(R.id.toolbar),
+                        withParent(IsInstanceOf.instanceOf(android.widget.LinearLayout::class.java))
+                    )
+                ),
+                isDisplayed()
+            )
+        )
+        linearLayoutCompat.check(matches(isDisplayed()))
+    }
+}
---
+++ app/src/main/java/com/example/vinilos_grupo27/network/NetworkServiceAdapter.kt
@@ -0,0 +1,70 @@
+package com.example.vinilos_grupo27.network
+
+import android.content.Context
+import android.util.Log
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.VolleyError
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.example.vinilos_grupo27.models.Album
+import com.example.vinilos_grupo27.models.Musician
+import org.json.JSONArray
+
+
+class NetworkServiceAdapter constructor(context: Context) {
+
+    companion object{
+        const val BASE_URL= "https://vynils-back2022.herokuapp.com/"
+        var instance: NetworkServiceAdapter? = null
+        fun getInstance(context: Context) =
+            instance ?: synchronized(this) {
+                instance ?: NetworkServiceAdapter(context).also {
+                    instance = it
+                }
+            }
+    }
+
+    private val requestQueue: RequestQueue by lazy {
+        Volley.newRequestQueue(context.applicationContext)
+    }
+
+    private fun getRequest(path:String, responseListener: Response.Listener<String>, errorListener: Response.ErrorListener): StringRequest {
+        return StringRequest(Request.Method.GET, BASE_URL+path, responseListener,errorListener)
+    }
+
+    fun getAlbums(onComplete:(resp:List<Album>)->Unit, onError: (error:VolleyError)->Unit){
+        requestQueue.add(getRequest("albums",
+            Response.Listener<String> { response ->
+                val resp = JSONArray(response)
+                val list = mutableListOf<Album>()
+                for (i in 0 until resp.length()) {
+                    val item = resp.getJSONObject(i)
+                    list.add(i, Album(albumId = item.getInt("id"),name = item.getString("name"), cover = item.getString("cover"), recordLabel = item.getString("recordLabel"), releaseDate = item.getString("releaseDate"), genre = item.getString("genre"), description = item.getString("description")))
+                }
+                onComplete(list)
+            },
+            Response.ErrorListener {
+                onError(it)
+            }))
+    }
+    fun getMusicians(onComplete:(resp:List<Musician>)->Unit, onError: (error:VolleyError)->Unit){
+        requestQueue.add(getRequest("musicians",
+            Response.Listener<String> { response ->
+                val resp = JSONArray(response)
+                val list = mutableListOf<Musician>()
+                for (i in 0 until resp.length()) {
+                    val item = resp.getJSONObject(i)
+                    list.add(i, Musician(albumId = item.getInt("id"),name = item.getString("name") ))
+                }
+                onComplete(list)
+                Log.d("musicos", list.toString())
+            },
+            Response.ErrorListener {
+                onError(it)
+            }))
+    }
+
+
+}
