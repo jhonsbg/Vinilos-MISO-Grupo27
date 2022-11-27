9066478d548472c0c84aef8f8297e5a98672e030
Nelson <n.ortizf@uniandes.edu.co>
2022-11-27 18:17:39 -0500

    navegacion de album detalle a asociar track

--- app/src/main/java/com/example/vinilos_grupo27/ui/AlbumDetailFragment.kt
+++ app/src/main/java/com/example/vinilos_grupo27/ui/AlbumDetailFragment.kt
@@ -10,6 +10,7 @@ import androidx.fragment.app.Fragment
 import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -43,6 +44,11 @@ class AlbumDetailFragment : Fragment() {
         recyclerView.layoutManager = LinearLayoutManager(context)
         recyclerView.adapter = viewModelAdapter
         Log.d("onViewCreated", "Aquí")
+
+        binding.addTrack.setOnClickListener(){
+            findNavController().navigate(R.id.action_albumDetailFragment_to_trackDetailFragment)
+        }
+
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
